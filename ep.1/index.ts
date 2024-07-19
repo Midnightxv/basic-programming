7c87c1dbc1a073907dfeb3a8eaf6e1e2d7f3b3a7
Midnight <99491186+Midnightxv@users.noreply.github.com>
2024-07-19 16:08:10 +0700

    ...

--- ep.1/index.ts
+++ ep.1/index.ts
@@ -6,3 +6,9 @@ let want_to_go_home : boolean = true
 console.log(fristname)
 console.log(y)
 console.log(want_to_go_home)
+
+const a : number = 2
+const b : number  = 5
+console.log(a===b)
+console.log(a>b)
+console.log(a<=b)
