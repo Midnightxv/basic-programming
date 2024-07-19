f8e32fa781573fb978883a264cb00d2ea693aaa8
Midnight <99491186+Midnightxv@users.noreply.github.com>
2024-07-19 16:28:48 +0700

    ..

--- ep.1/index.ts
+++ ep.1/index.ts
@@ -1,14 +1,14 @@
-const fristname : string = "ณัฐชาภรณ์"
-let y : number = 10
+const fristName: string = "natchaporn"
+let y: number = 10
 y = 20
-let want_to_go_home : boolean = true
+let wantToGoHome: boolean = true
 
-console.log(fristname)
+console.log(fristName)
 console.log(y)
-console.log(want_to_go_home)
+console.log(wantToGoHome)
 
-const a : number = 2
-const b : number  = 5
-console.log(a===b)
-console.log(a>b)
-console.log(a<=b)
+const a: number = 2
+const b: number  = 5
+console.log(a === b)
+console.log(a > b)
+console.log(a <= b)
