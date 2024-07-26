3619a57b85e4c877456c4f2f16fc69f5c5674cc6
Midnight <99491186+Midnightxv@users.noreply.github.com>
2024-07-26 18:41:45 +0700

    ..

---
+++ Ep..3/Homework.ts
@@ -0,0 +1,14 @@
+function bmi(height: number, weight: number): string {
+    let bmi = weight / (height * height);
+
+    if (bmi < 18.5) {
+        return 'Underweight';
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+        return 'Normal weight';
+    } else if (bmi >= 25 && bmi < 29.9) {
+        return 'Overweight';
+    } else {
+        return 'Obesity';
+    }
+}
+console.log(bmi(155,55)
