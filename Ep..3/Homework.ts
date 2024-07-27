0ffda6e97508fc011c8ae794cbbbf52c182e5f28
Midnight <99491186+Midnightxv@users.noreply.github.com>
2024-07-27 20:01:44 +0700

    ...

--- Ep..3/Homework.ts
+++ Ep..3/Homework.ts
@@ -1,14 +1,18 @@
-function bmi(height: number, weight: number): string {
-    let bmi = weight / (height * height);
+function bmi(height: number , weight: number) {
+    let bmi = weight /  height * height ;
 
     if (bmi < 18.5) {
-        return 'Underweight';
-    } else if (bmi >= 18.5 && bmi < 24.9) {
+        return 'Underweight' ;
+    } else if (bmi >= 18.5 && bmi < 22.9) {
         return 'Normal weight';
-    } else if (bmi >= 25 && bmi < 29.9) {
-        return 'Overweight';
+    } else if (bmi >= 23 && bmi < 24.9) {
+        return 'buxom';
+    } else  if (bmi >= 25 && bmi < 29.9) {
+        return 'Fat';
+    } else if (bmi > 30) {
+        return 'Very fat'
     } else {
-        return 'Obesity';
+        return 'Error'
     }
 }
-console.log(bmi(155,55)
+console.log(bmi(1.55,55)
