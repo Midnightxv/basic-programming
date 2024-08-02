3f2940d438854270dd233f728c45d2a2700282f7
Midnight <99491186+Midnightxv@users.noreply.github.com>
2024-08-02 14:01:12 +0700

    ////

--- Ep..3/Homework.ts
+++ Ep..3/Homework.ts
@@ -15,4 +15,4 @@ function bmi(height: number , weight: number) {
         return 'Error'
     }
 }
-console.log(bmi(1.55,55)
+console.log(bmi(1.55,55))
---
+++ ep.4/Index.ts
@@ -0,0 +1,66 @@
+function SayHi(dayOfWeek: number){
+    if(dayOfWeek===1){
+        console.log('สวัสดีวันอาทิตย์')
+    } else if (dayOfWeek===2){
+        console.log('สวัสดีวันจันทร์')
+    } else if (dayOfWeek===3){
+        console.log('สวัสดีวันอังคาร')
+    } else if (dayOfWeek===4){
+        console.log('สวัสดีวันพุธ')
+    } else if (dayOfWeek===5) {
+        console.log('สวัสดีวันพฤหัสบดี')
+    } else if (dayOfWeek===6) {
+        console.log('สวัสดีวันศุกร์')
+    } else if (dayOfWeek===7) {
+        console.log('สวัสดีวันเสาร์')
+    } 
+}
+d(6)
+
+function SayHi(dayOfWeek: number){
+    switch(dayOfWeek){
+        case 1 :
+        console.log('สวัสดีวันอาทิตย์')
+        break
+        case 2 :
+        console.log('สวัสดีวันจันทร์')
+        break
+        case 3 :
+        console.log('สวัสดีวันอังคาร')
+        break
+        case 4 :
+        console.log('สวัสดีวันพุธ')
+        break
+        case 5 :
+        console.log('สวัสดีวันพฤหัสบดี')
+        break
+        case 6 :
+        console.log('สวัสดีวันศุกร์')
+        break
+        case 7 :
+        console.log('สวัสดีวันเสาร์')
+        break
+    }
+}
+SayHi(5)
+
+function shirt(size: string){
+    switch (size){
+        case 's' :
+        console.log('ผอม')
+        break
+        case 'm' :
+        console.log('ผอม')
+        break
+        case 'l' :
+        console.log('ท้วม')
+        break
+        case 'xl' :
+        console.log('อ้วน')
+        break
+        case '2xl' :
+        console.log('อ้วน')
+        break
+    }
+}
+shirt('2xl')
