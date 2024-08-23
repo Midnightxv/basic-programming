5c40fa5c8cac4f2279c6db756b7c48f2d59840ea
Midnight <99491186+Midnightxv@users.noreply.github.com>
2024-08-23 13:54:33 +0700

    dfvdsf

---
+++ Ep.6/Index.ts
@@ -0,0 +1,13 @@
+// let sum: number = 0
+
+// for (let i = 1; i <= 31; i+++){
+//      sum = sum + 1
+// }
+
+// console.log('Total: ', sum)
+
+let sum: number = 0
+
+while (i <= 31){
+    sum = sum + i
+}
