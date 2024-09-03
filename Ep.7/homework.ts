a8806e2b7654178210f415ef967df8de80595460
Midnight <99491186+Midnightxv@users.noreply.github.com>
2024-09-03 08:08:47 +0700

    sdfgvsrd

---
+++ Ep.7/homework.ts
@@ -0,0 +1,13 @@
+const personalInfomation ={
+    name : 'Methee Heephong',
+    addessNowNo : '22',
+    villageNo : '-',
+    street : 'usa',
+    district : 'Dorson',
+    county: 'Ruban',
+    province: 'izad',
+    zipCode : '13130',
+    phone : '0987654321',
+    emaill : 'Kutainae isas'
+}
+console.log (personalInfomation)
