9acc8ccbc3721ff08891d494a497683e443908eb
Midnight <99491186+Midnightxv@users.noreply.github.com>
2024-09-06 14:07:17 +0700

    fgd

---
+++ Ep.8/Index.ts
@@ -0,0 +1,46 @@
+const aboutMe = {
+    firstName: 'View',
+    lastName: 'Super',
+    age: 14,
+    mail: 'mmmof*.com',
+    phone: '07xxx87887',
+    idCard: 'xx345xxxx',
+    fan: {
+        firstName: 'Lisa',
+        lastName: 'Mamamoosub',
+        age: 15,
+        animal:{
+            animalType: 'cat',
+            color: 'orange'
+        }
+    },
+    myBro: {
+    firstName: 'Lisa',
+    lastName:'Mamamoosub',
+    AKA:'SuperMan',
+    }
+}
+console.log(aboutMe.myBro.AKA)
+
+const myFriend = [{
+    firstName: 'Lisa',
+    lastName:'Mamamoosub',
+    AKA:'SuperMan',
+    age:16
+}, {
+     firstName: 'Rose',
+    lastName:'Mamamoosub',
+    AKA:'RoseOnPink',
+    age: 20
+}, {
+     firstName: 'Jisoo',
+    lastName:'Mamamoosub',
+    AKA:'Jinny',
+    age: 24
+},{
+     firstName: 'Jenny',
+    lastName:'Mamamoosub',
+    AKA:'WonderG',
+    age: 17
+}]
+console.log(myFriend[0].firstName)
