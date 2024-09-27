0c7e51f5d0192c3f515815e1fde564fec40d8981
Midnight <99491186+Midnightxv@users.noreply.github.com>
2024-09-27 14:30:38 +0700

    sdc

--- Ep.11/Index.ts
+++ Ep.11/Index.ts
@@ -23,3 +23,22 @@ login('max@uyk.ac.th','max@utk1234')
 /////เข้าสู่ระบบที่มีจีเมลอยู่ในระบบอยู่เเล้ว////////
 ////////////////////////////////////////
 
+function login(email:string,password: string){
+    const user = database.filter(function(element,index){
+        return element. email === email
+    }) 
+    if (email.includes("@") && email.lastIndexOf('@') !== email.length - 1) {
+        if (user.length > 0) {
+            if (atob(user[0].password) === password){
+                alert('เข้าสู้ระบบเเล้ว')
+            }else{
+                alert('รหัสผ่านไม่ถูกต้อง')
+            } 
+        } else {
+            alert('ไม่มีผู้ใช้งานในระบบ')
+        }
+    } else {
+        alert('Email ไม่ถูกต้อง')
+    }
+}
+//////////////อีกเเบบ//////////////////////////
